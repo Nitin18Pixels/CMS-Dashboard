f18e794ab5c9fb34980736681f2733c8c9969cba
Nitin Sharma <nitin2019it035abesit@gmail.com>
2025-03-12 08:29:17 +0530

    sidebar align

--- src/components/common/sidebar/index.jsx
+++ src/components/common/sidebar/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { FaAngleDown, FaAngleRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { MenuTypes } from "./Menu";
 import "../../../layout/styles.css";
 
 const Sidebar = ({ isOpen }) => {
   const [openDropdown, setOpenDropdown] = useState(null);
+  const location = useLocation();
 
   const toggleDropdown = (index) => {
     setOpenDropdown(openDropdown === index ? null : index);
@@ -13,37 +14,98 @@ const Sidebar = ({ isOpen }) => {
 
   return (
     <div
-      className={`bg-dark text-white p-3 sidebar ${isOpen ? "d-block" : "d-none d-md-block"}`}
-      style={{ width: isOpen ? "250px" : "80px", transition: "width 0.3s ease-in-out", overflowX: "hidden" }}
+      className={`bg-dark text-white p-3 sidebar ${
+        isOpen ? "d-block" : "d-none d-md-block"
+      }`}
+      style={{
+        width: isOpen ? "250px" : "80px",
+        transition: "width 0.3s ease-in-out",
+        overflowX: "hidden",
+      }}
     >
       <h4 className="text-center">{isOpen ? "Dashboard" : <FaAngleRight />}</h4>
       <ul className="list-unstyled">
-        {MenuTypes.map((menu, index) => (
-          <li key={menu.id} className="p-2">
-            <Link to={menu.link} className="text-white text-decoration-none">
-              {menu.icon} {isOpen && menu.menu}
-            </Link>
-            {menu.submenu && (
-              <>
-                <div onClick={() => toggleDropdown(index)} style={{ cursor: "pointer" }}>
-                  {isOpen && (openDropdown === index ? <FaAngleDown /> : <FaAngleRight />)}
-                </div>
-                <ul
-                  className={`list-unstyled ps-3 ${openDropdown === index && isOpen ? "dropdown-open" : "dropdown-closed"}`}
-                  style={{ transition: "max-height 0.3s ease-in-out", overflow: "hidden", maxHeight: openDropdown === index && isOpen ? "500px" : "0", position: "relative", top: "0" }}
+        {MenuTypes.map((menu, index) => {
+          // Check if the menu or any of its submenu is active
+          const isActive =
+            location.pathname === menu.link ||
+            (menu.submenu &&
+              menu.submenu.some((submenu) => location.pathname === submenu.link));
+
+          return (
+            <li
+              key={menu.id}
+              className={`p-2 ${isActive ? "active-menu" : ""}`}
+            >
+              {/* Remove Link if submenu exists */}
+              {menu.submenu ? (
+                <div
+                  className="d-flex justify-content-between align-items-center"
+                  onClick={() => toggleDropdown(index)}
+                  style={{ cursor: "pointer" }}
                 >
-                  {menu.submenu.map((submenu) => (
-                    <li key={submenu.submenuID} className="p-2">
-                      <Link to={submenu.link} className="text-white text-decoration-none">
-                        {submenu.label}
-                      </Link>
-                    </li>
-                  ))}
+                  <div>
+                    {menu.icon} {isOpen && menu.menu}
+                  </div>
+                  {isOpen &&
+                    (openDropdown === index ? (
+                      <FaAngleDown />
+                    ) : (
+                      <FaAngleRight />
+                    ))}
+                </div>
+              ) : (
+                <Link
+                  to={menu.link}
+                  className={`text-white text-decoration-none ${
+                    isActive ? "active-link" : ""
+                  }`}
+                >
+                  {menu.icon} {isOpen && menu.menu}
+                </Link>
+              )}
+
+              {/* Submenu */}
+              {menu.submenu && (
+                <ul
+                  className={`list-unstyled ps-3 ${
+                    openDropdown === index && isOpen
+                      ? "dropdown-open"
+                      : "dropdown-closed"
+                  }`}
+                  style={{
+                    maxHeight:
+                      openDropdown === index && isOpen ? "500px" : "0",
+                    overflow: "hidden",
+                    transition: "max-height 0.3s ease",
+                  }}
+                >
+                  {menu.submenu.map((submenu) => {
+                    const isSubmenuActive =
+                      location.pathname === submenu.link;
+                    return (
+                      <li
+                        key={submenu.submenuID}
+                        className={`p-2 ${
+                          isSubmenuActive ? "active-submenu" : ""
+                        }`}
+                      >
+                        <Link
+                          to={submenu.link}
+                          className={`text-white text-decoration-none ${
+                            isSubmenuActive ? "active-link" : ""
+                          }`}
+                        >
+                          {submenu.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
                 </ul>
-              </>
-            )}
-          </li>
-        ))}
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
